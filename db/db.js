aa81de72bff3c30a81ce8296211520842d9f0c77
Shreyansh Jaiswal <jshreyansh459@gmail.com>
2023-06-29 23:05:51 +0530

    files added

---
+++ db/db.js
@@ -0,0 +1,310 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"Hi &#128075;",
+				"I'm Shreyansh Jaiswal. I’m currently a student at Jaypee University of Engineering & Technology specialized in Computer Science and Engineering.",
+				"I'm a Web Developer, geek and curious human besides being an DSA enthusiast. I have experience of delivering solutions for interesting problems.",
+				"I love to read, listen to music, and travel.",
+			],
+		},
+		contact: {
+			text: [
+				"If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email. My email address is: jshreyansh459@gmail.com",
+			],
+		},
+	},
+	skills: [
+		{
+			title: "Languages",
+			skillName: "C, C++",
+			color: "1",
+			percentage: "90",
+		},
+		{
+			title: "Frameworks/Libraries",
+			skillName: "Reactjs",
+			color: "6",
+			percentage: "80",
+		},
+		{
+			title: "Backend",
+			skillName: "Nodejs, MongoDB",
+			color: "2",
+			percentage: "50",
+		},
+		{
+			title: "Clouds",
+			skillName: "Google Cloud, Heroku, Netlify",
+			color: "3",
+			percentage: "30",
+		},
+		{
+			title: "Design",
+			skillName: "HTML, Bootstrap, CSS",
+			color: "4",
+			percentage: "90",
+		},
+		{
+			title: "Version Control",
+			skillName: "Git, GitHub",
+			color: "7",
+			percentage: "70",
+		},
+		{
+			title: "Tools",
+			skillName: "Postman, Chrome DevTools",
+			color: "3",
+			percentage: "80",
+		},
+		/*{
+			title: "Saas products",
+			skillName: "CleverTap, FreshDesk",
+			color: "5",
+			percentage: "50",
+		},*/
+		{
+			title: "Editor",
+			skillName: "Visual Code Studio, Atom",
+			color: "6",
+			percentage: "80",
+		},
+	],
+	projects: {
+		web: [
+			 {
+			 	projectName: "Programming Diaries",
+			 	image: "images/programmingdiaries.png",
+			 	summary:
+			 		"Developed a full stack blog application to provide content on techical topics across the internet with admin interface.",
+			 	preview: "https://programmingdiaries.herokuapp.com/",
+			 	techStack: [
+			 		"Django",
+			 		"SQLite",
+			 		"Bootstrap",
+			 		"JavaScript",
+			 		"Heroku",
+			 	],
+			 }
+			// {
+			// 	projectName: "Find Your Bank",
+			// 	image: "images/findyourbank.png",
+			// 	summary:
+			// 		"Developed a React application to render a list of banks fetched from API. Filtered the banks based on queries from localstorage, marked favorites banks.",
+			// 	preview: "https://clever-fermi-0d5d76.netlify.app",
+			// 	techStack: ["Reactjs", "Bootstrap", "JavaScript", "Netlify"],
+			// },
+			// {
+			// 	projectName: "Web Portfolio",
+			// 	image: "images/portfolio.png",
+			// 	summary:
+			// 		"Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
+			// 	preview: "https://github.com/vinaysomawat/Travographer-Portal",
+			// 	techStack: ["HTML", "Bootstrap", "JavaScript"],
+			// },
+			// {
+			// 	projectName: "Resume Builder",
+			// 	image: "images/resume-builder.png",
+			// 	summary:
+			// 		"Browser based editor to build and download Resumes in a customizable templates.",
+			// 	preview: "https://vinaysomawat.github.io/Resume-Builder",
+			// 	techStack: ["HTML", "Bootstrap", "JavaScript"],
+			// },
+		],
+		software: [
+			// {
+			// 	projectName: "Pizza Ordering ChatBot",
+			// 	image: "images/pizzaorderchatbot.png",
+			// 	summary:
+			// 		"ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
+			// 	preview:
+			// 		"https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
+			// 	techStack: ["Dailogflow", "Firebase"],
+			// },
+			// {
+			// 	projectName: "WhatsApp-Bot",
+			// 	image: "images/whatsappbot.jpg",
+			// 	summary:
+			// 		"Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
+			// 	preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
+			// 	techStack: ["Selenium", "Chrome Webdriver", "Python"],
+			// },
+			// {
+			// 	projectName: "Bill Generator",
+			// 	image: "images/billgenerator.png",
+			// 	summary:
+			// 		"GUI to transfer data to excel sheets and generate bills on the local shops.",
+			// 	preview: "https://github.com/vinaysomawat/Bill-Generator",
+			// 	techStack: ["Tkinter", "Openxlpy", "Python"],
+			// },
+		],
+		android: [
+			 {
+			 	projectName: "AI baseed Attendance System",
+			 	image: "images/nitwcse.jpg",
+			 	summary:
+			 		"The Application shows the real time attendance of all the registered students with exact timeing of particular date.",
+			 	preview: "https://github.com/shreyaaansh/AI-based-Attendance-System",
+			 	techStack: ["Python","OpenCV","NumPy", "Face-Recognition", "CSV"],
+			 },
+			 //{
+			 //	projectName: "CareerHigh-App",
+			 //	image: "images/carrerhigh.png",
+			 //	summary:
+			 //		"The Application display the webpages of website careerhigh.in in android devices.",
+			 //	preview: "https://github.com/vinaysomawat/CareerHigh-Android",
+			 //	techStack: ["JAVA", "XML", "Android"],
+			 //},
+		],
+		freelance: [
+			// {
+			// 	projectName: "SnylloAir.com",
+			// 	image: "images/snylloair.png",
+			// 	summary:
+			// 		"Developed a company website to showcase the purpose, services and products provided by the company to audience.",
+			// 	preview: "https://www.snylloair.com/",
+			// 	techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
+			// },
+			// {
+			// 	projectName: "Delivery+",
+			// 	image: "images/AM-Logo-.png",
+			// 	summary:
+			// 		"Android Application to display website in android devices.",
+			// 	preview:
+			// 		"https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
+			// 	techStack: ["Android", "JAVA", "Play Store"],
+			// },
+		],
+	},
+	experience: [
+		 {
+		 	title: "Banaras Locomotive Works",
+		 	duration: "June 2023 - July 2023",
+		 	subtitle: "Computer Science Intern",
+		 	details: [
+		 		"Working on the products in the healthcare/digital therapeutics domain."
+		 	],
+		 	tags: [
+		 		"JavaScript",
+		 		"Angular",
+		 		"Bootstrap",
+		 		"Nodejs",
+		 		"Jenkins",
+		 	],
+			icon: "heartbeat",
+		}
+		// {
+		// 	title: "Novopay Solutions Pvt. Ltd.",
+		// 	duration: "June 2020 - April 2022",
+		// 	subtitle: "Software Engineer",
+		// 	details: [
+		// 		"",
+		// 		
+		// 	],
+		// 	tags: [
+		// 		"JavaScript",
+		// 		"Angular",
+		// 		"React",
+		// 		"Bootstrap",
+		// 		"Nodejs",
+		// 		"Jenkins",
+		// 	],
+		// 	icon: "qrcode",
+		// },
+		
+	],
+	education: [
+		{
+			title: "Bachelors in Computer Science and Engineering",
+			duration: "",
+			subtitle: "Jaypee University of Engineering and Technology, Guna",
+			details: [
+				"Current CGPA 7.8",
+				"Active Competitive Programmer with CodeChef and Leetcode ratings*.",
+				"Head of lots of college's different types of clubs",
+			],
+			tags: [
+				"Data Structures & Algorithms",
+				"Operating Systems",
+				"Database Management System",
+				"Computer Networks",
+				"Mobile Computing",
+				"Artificial Intelligence",
+				"Theory Of Computation",
+			],
+			icon: "graduation-cap",
+		},
+		{
+			title: "Class 11-12th in Science and Mathematics",
+			duration: "",
+			subtitle: "Sunbeam English School, Bhagwanpur",
+			details: [
+				"Central Board of Secondary Education",
+				"Secured 80 percentage in Class 12th Boards Examinations.",
+			],
+			tags: ["Physics", "Chemistry", "Mathematics"],
+			icon: "book",
+		},
+	],
+	footer: [
+		{
+			label: "Dev Profiles",
+			data: [
+				{
+					text: "LinkedIn",
+					link: "https://www.linkedin.com/in/shreyaaansh/",
+				},
+				{
+					text: "GitHub",
+					link: "https://github.com/shreyaaansh",
+				},
+				{
+					text: "LeetCode",
+					link: "https://leetcode.com/shreyaansh_6/",
+				},
+			],
+		},
+		{
+			label: "Resources",
+			data: [
+				{
+					text: "Enable Dark/Light Mode",
+					func: "enableDarkMode()",
+				},
+				// {
+				// 	text: "Print this page",
+				// 	func: "window.print()",
+				// },
+			],
+		},
+		{
+			label: "Social Profiles",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://www.linkedin.com/in/shreyaaansh/",
+				},
+				{
+					text: "Gmail",
+				 	link: "jshreyansh459@gmail.com",
+				},
+				{
+				 	text: "Instagram",
+				 	link: "shreyaaansh_",
+				},
+				{
+					text: "Buy me a coffee",
+					link: "https://ko-fi.com/I3I8FY7H2#paypalModal",
+				},
+			],
+		},
+		{
+			label: "copyright-text",
+			data: [
+				"",
+				"Made with &hearts;",
+				"by Shreyansh Jaiswal."
+			],
+		},
+	],
+};
